44436d2e253ae16033763b1ef2b969381a20b7e1
ahmedfhmu <69735147+ahmedfhmu@users.noreply.github.com>
2020-08-22 19:39:42 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+$(window).load(function() {
+
+	
+
+    $(".sk-cube-grid").fadeOut(2000, 
+    function() {
+
+        $(this).parent().fadeOut(2000, 
+        function() {
+
+
+        
+    
+});
+});
+});
