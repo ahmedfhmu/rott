663fd3187d5a3ecea879877e3cb85d78a8178823
ahmedfhmu <69735147+ahmedfhmu@users.noreply.github.com>
2020-08-24 06:31:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+$(window).load(function() {
+
+
+    $(".sk-cube-grid").fadeOut(1000, 
+        
+    function() {
+
+        $(this).parent().fadeOut(1000, 
+        function() {
+
+            $("body").css("overflow","auto");
+
+        
+    
+});
+});
+});
+
